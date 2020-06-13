6687072c6768737d7c6e49c2e29e2b4ffd1509d4
James Kaviyil Jose <jamesjose03@gmail.com>
2020-06-13 21:26:01 +0530

    feat: completed Sign Up Route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@ Welcome!
     <div class="row">
         <div class="col-6">
             <h2>Sign Up</h2>
-            <form action="#" method="post">
+            <form action="{{ route('signup') }}" method="post">
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input class="form-control" type="text" name="email" id="email">
@@ -22,6 +22,7 @@ Welcome!
                     <input class="form-control" type="password" name="password" id="password">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
             </form>
         </div>
         <div class="col-6">
