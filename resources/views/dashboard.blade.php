610db47ef52ddf2deef669b9e3b16cd53c4e7b4d
James Kaviyil Jose <jamesjose03@gmail.com>
2020-06-14 19:04:47 +0530

    feat: add styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,45 @@
-<h1>Dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+
+<section class = "row new-post">
+    <div class="col-md-6 col-md-offset-3">
+        <header><h2>Write a new post...</h2></header>
+        <form action="">
+            <div class="form-group">
+                <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Type here"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Post</button>
+        </form>
+    </div>
+</section>
+<section class="row posts">
+    <div class="col-md-6 col-md-offset-3">
+        <header><h3>Other posts:</h3></header>
+        <article class="post">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente minima rerum nesciunt ipsam ex dolor.</p>
+        <div class="info">
+            Posted by JKJ on 10th Jun 2020
+        </div>
+        <div class="interaction">
+        <a href="">Like</a>
+        <a href="">Dislike</a>
+        <a href="">Edit</a>
+        </div>
+        </article>
+        <article class="post">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente minima rerum nesciunt ipsam ex dolor.</p>
+        <div class="info">
+            Posted by JKJ on 10th Jun 2020
+        </div>
+        <div class="interaction">
+        <a href="">Like</a>
+        <a href="">Dislike</a>
+        <a href="">Edit</a>
+        </div>
+        </article>
+    </div>
+    
+</section>
+
+@endsection
